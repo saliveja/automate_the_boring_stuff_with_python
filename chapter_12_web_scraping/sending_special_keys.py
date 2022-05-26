ac9a84685ecc4f5c592075b0c065a301fe0643a8
saliveja <esman@riseup.net>
2022-05-26 11:16:56 +0200

    navigating a page with selenium

---
+++ chapter_12_web_scraping/sending_special_keys.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
+browser = webdriver.Firefox()
+browser.get('https://nostarch.com')
+htmlElem = browser.find_element(By.TAG_NAME, 'html')
+htmlElem.send_keys(Keys.END)
+# scrolls down to the end of the page
+
+time.sleep(5)
+# pauses for five seconds
+
+htmlElem.send_keys(Keys.HOME)
+# returns to home
