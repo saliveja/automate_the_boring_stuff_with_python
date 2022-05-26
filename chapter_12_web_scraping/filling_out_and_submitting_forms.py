684d35683f6effd75404c846f28bd59b1c69109a
saliveja <esman@riseup.net>
2022-05-26 10:16:42 +0200

    trying program for another page

--- chapter_12_web_scraping/filling_out_and_submitting_forms.py
+++ chapter_12_web_scraping/filling_out_and_submitting_forms.py
@@ -3,10 +3,14 @@ from selenium.webdriver.common.by import By
 
 browser = webdriver.Firefox()
 
-browser.get('https://login.metafilter.com')
-userElem = browser.find_element(By.ID, 'user_name')
+browser.get('https://mail.com')
+
+linkElem = browser.find_element(By.ID, 'login-button')
+linkElem.click()
+
+userElem = browser.find_element(By.ID, 'login-email')
 userElem.send_keys('your_real_username_here')
 
-passwordElem = browser.find_element(By.ID, 'user_pass')
+passwordElem = browser.find_element(By.ID, 'login-password')
 passwordElem.send_keys('your_real_password_here')
 passwordElem.submit()
