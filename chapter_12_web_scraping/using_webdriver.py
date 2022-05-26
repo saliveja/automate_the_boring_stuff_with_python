2208bd72cc4d05a869a36df3e309d094161eb8df
saliveja <esman@riseup.net>
2022-05-26 09:53:15 +0200

    automatize clicking link

--- chapter_12_web_scraping/using_webdriver.py
+++ chapter_12_web_scraping/using_webdriver.py
@@ -1,11 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-browser = webdriver.Firefox()
-browser.get('https://inventwithpython.com')
+driver = webdriver.Firefox()
+driver.get('https://inventwithpython.com')
+#
+# try:
+#     elem = driver.find_element(By.CLASS_NAME, 'container')
+#     print('Found <%s> element with that class name!' % f'{elem.tag_name}')
+#
+# except:
+#     print('Was not able to find and element with that name.')
 
-try:
-    elem = driver.find_element(By.CLASS_NAME, ' .cover-thumb')
-    print('Found <%s> element with that class name!' % f'{elem.tag_name}')
-except:
-    print('Was not able to find and element with that name.')
+linkElem = driver.find_element(By.LINK_TEXT, 'Read Online for Free')
+
+print(type(linkElem))
+
+linkElem.click()
