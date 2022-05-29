b852728909b7c80bb979870d7f1877084e347319
saliveja <esman@riseup.net>
2022-05-29 11:04:18 +0200

    translating column numbers to letters

---
+++ Chapter_13_working_with_spreadsheets/converting_between_column_letters_and_numbers.py
@@ -0,0 +1,8 @@
+import openpyxl
+from openpyxl.utils import get_column_letter, column_index_from_string
+
+print(get_column_letter(1))
+# translating column one to a letter --> A
+
+print(get_column_letter(2))
+# translating column one to a letter --> B
