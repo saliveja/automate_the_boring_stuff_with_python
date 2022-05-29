951c061fa1301c32fbcc4854d3ad7cb0597ffbd1
saliveja <esman@riseup.net>
2022-05-29 10:07:59 +0200

    getting sheets from the workbook

--- Chapter_13_working_with_spreadsheets/opening_excel_doc_wt_openpyxl.py
+++ Chapter_13_working_with_spreadsheets/opening_excel_doc_wt_openpyxl.py
@@ -1,6 +1,7 @@
 import openpyxl
 import os
 
+# opening excel docs with openpyxl
 wb = openpyxl.load_workbook('example.xlsx')
 # returns value of workbook data type
 print(type(wb))
@@ -9,3 +10,22 @@ dir = os.getcwd()
 # returns the working directory
 # we can use os.chdir() to change directory
 print(dir)
+
+# getting sheets from the workbook
+print(wb.sheetnames)
+# ['Sheet1', 'Sheet2', 'Sheet3']
+
+sheet = wb['Sheet3']
+print(sheet)
+# returns: <Worksheet "Sheet3">
+
+print(type(sheet))
+# returns: <class 'openpyxl.worksheet.worksheet.Worksheet'>
+
+print(sheet.title)
+# gets the title of the sheet
+
+anotherSheet = wb.active
+# getting the active sheet
+
+print(anotherSheet)
