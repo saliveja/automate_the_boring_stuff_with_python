842be7ec151fa6e65f8838074e9125c550df9365
saliveja <esman@riseup.net>
2022-05-29 14:07:41 +0200

    converting between letters and numbers

--- Chapter_13_working_with_spreadsheets/converting_between_column_letters_and_numbers.py
+++ Chapter_13_working_with_spreadsheets/converting_between_column_letters_and_numbers.py
@@ -6,3 +6,21 @@ print(get_column_letter(1))
 
 print(get_column_letter(2))
 # translating column one to a letter --> B
+
+print(get_column_letter(27))
+# AA
+
+print(get_column_letter(900))
+# AHP
+
+wb = openpyxl.load_workbook('example.xlsx')
+sheet = wb['Sheet1']
+
+print(get_column_letter(sheet.max_column))
+# C (there are three columns in total, so max is C)
+
+print(column_index_from_string('A'))
+# 1
+
+print(column_index_from_string('AA'))
+# 27
