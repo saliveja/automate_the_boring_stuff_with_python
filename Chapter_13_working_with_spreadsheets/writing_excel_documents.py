6dfab803174f025d50cdae72c37bbbbc0fad4dcb
saliveja <esman@riseup.net>
2022-05-30 10:08:19 +0200

    changing name on sheet, creating copy of file with changes

---
+++ Chapter_13_working_with_spreadsheets/writing_excel_documents.py
@@ -0,0 +1,21 @@
+import openpyxl
+
+wb = openpyxl.Workbook()
+# creates a new workbook
+print(wb.sheetnames)
+# name is default 'Sheet'
+sheet = wb.active
+# sheet is defined as the active sheet
+print(sheet.title)
+# printing title as string
+sheet.title = 'Spam Bacon Eggs Sheet'
+print(wb.sheetnames)
+print(sheet.title)
+
+
+# changing name on sheet
+# import openpyxl
+# wb=openpyxl.load_workbook('example.xlsx')
+# sheet=wb.active
+# sheet.title='Spam Spam Spam'
+# wb.save('example_copy.xlsx')
